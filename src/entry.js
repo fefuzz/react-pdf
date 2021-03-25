8002fb69aebfa1e3e756ef7caf0ffed5426047cd
Pierpaolo Vanni <pierpaolo@MacBook-Pro-di-Pierpaolo.local>
2021-03-25 11:24:25 +0100

    build

--- src/entry.js
+++ src/entry.js
@@ -9,7 +9,7 @@ if (isLocalFileSystem) {
   warnOnDev('You are running React-PDF from your local file system. PDF.js Worker may fail to load due to browser\'s security policies. If you\'re on Google Chrome, you can use --allow-file-access-from-files flag for debugging purposes.');
 }
 
-pdfjs.GlobalWorkerOptions.workerSrc = 'pdf.worker.entry.js';
+pdfjs.GlobalWorkerOptions.workerSrc = 'pdf.worker.js';
 
 export {
   pdfjs,
